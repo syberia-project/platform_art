hopez13/art

Catch clauses should only have throwable types.
Test: test.py
Bug: 28313047
Change-Id: I48bc06b408e4f80cc52293e0c71331d2e15f753d

runtime/verifier/method_verifier.cc
@@ -3704,7 +3704,7 @@ bool MethodVerifier<kVerifierDebug>::HandleMoveException(const Instruction* inst
                     unresolved = &unresolved->SafeMerge(exception, &reg_types_, this);
                   }
                 } else {
-                  Fail(VERIFY_ERROR_BAD_CLASS_SOFT) << "unexpected non-exception class "
+                  Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "unexpected non-exception class "
                                                     << exception;
                   return std::make_pair(true, &reg_types_.Conflict());
                 }

syberia-project/platform_art

runtime/verifier/method_verifier.cc
@@ -3817,7 +3817,7 @@ bool MethodVerifier<kVerifierDebug>::HandleMoveException(const Instruction* inst
                     unresolved = &unresolved->SafeMerge(exception, &reg_types_, this);
                   }
                 } else {
-                  Fail(VERIFY_ERROR_BAD_CLASS_SOFT) << "unexpected non-exception class "
+                  Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "unexpected non-throwable class "
                                                     << exception;
                   return std::make_pair(true, &reg_types_.Conflict());
                 }