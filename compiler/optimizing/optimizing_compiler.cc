bionic-linux/android_art

ART: Add two extra DCE passes
Add two DCE passes to eliminate dead instructions left by:
- Loop optimization unrolling. After loop optimization, dead
  instructions are often left that, when removed can simplify the
  structure of the loop and remove basic blocks. This extra DCE pass
  creates an opportunity for the load store elimination pass to
  remove some loads/stores.
- The last instruction simplification pass.

Authors: Hari Limaye, Chris Jones

Test: 458-checker-instruct-simplification
Test: 530-checker-lse, 530-checker-lse-simd, 706-checker-scheduler
Test: test-art-target, test-art-host
Change-Id: I6e14cbfc3c320af5f507135a067bb7b5bc4df8f4

compiler/optimizing/optimizing_compiler.cc
@@ -666,6 +666,9 @@ void OptimizingCompiler::RunOptimizations(HGraph* graph,
     OptDef(OptimizationPass::kBoundsCheckElimination),
     OptDef(OptimizationPass::kLoopOptimization),
     // Simplification.
+    OptDef(OptimizationPass::kDeadCodeElimination,
+           "dead_code_elimination$after_loop_opt",
+           OptimizationPass::kLoopOptimization),
     OptDef(OptimizationPass::kConstantFolding,
            "constant_folding$after_bce"),
     OptDef(OptimizationPass::kAggressiveInstructionSimplifier,
@@ -681,6 +684,11 @@ void OptimizingCompiler::RunOptimizations(HGraph* graph,
     // HTypeConversion from a type to the same type.
     OptDef(OptimizationPass::kAggressiveInstructionSimplifier,
            "instruction_simplifier$before_codegen"),
+    // Simplification may result in dead code that should be removed prior to
+    // code generation.
+    OptDef(OptimizationPass::kDeadCodeElimination,
+           "dead_code_elimination$before_codegen",
+           OptimizationPass::kAggressiveInstructionSimplifier),
     // Eliminate constructor fences after code sinking to avoid
     // complicated sinking logic to split a fence with many inputs.
     OptDef(OptimizationPass::kConstructorFenceRedundancyElimination)

syberia-project/platform_art

Signed-off-by: DennySPb <dennyspb@gmail.com>

compiler/optimizing/optimizing_compiler.cc
@@ -670,11 +670,14 @@ void OptimizingCompiler::RunOptimizations(HGraph* graph,
            "constant_folding$after_bce"),
     OptDef(OptimizationPass::kAggressiveInstructionSimplifier,
            "instruction_simplifier$after_bce"),
-    // Other high-level optimizations.
-    OptDef(OptimizationPass::kLoadStoreElimination),
-    OptDef(OptimizationPass::kCHAGuardOptimization),
     OptDef(OptimizationPass::kDeadCodeElimination,
            "dead_code_elimination$after_bce"),
+    // Other high-level optimizations.
+    OptDef(OptimizationPass::kLoadStoreElimination),
+    OptDef(OptimizationPass::kDeadCodeElimination,
+           "dead_code_elimination$after_lse",
+           OptimizationPass::kLoadStoreElimination),
+    OptDef(OptimizationPass::kCHAGuardOptimization),
     OptDef(OptimizationPass::kCodeSinking),
     // The codegen has a few assumptions that only the instruction simplifier
     // can satisfy. For example, the code generator does not expect to see a