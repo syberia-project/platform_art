8487cb2270527e80617447276d950df00df97869
John Galt <johngaltfirstrun@gmail.com>
2024-06-04 10:21:46 -0400

    libartservice: change default dexopt concurrency to 6
    Multiple thread blocking is far too common

--- libartservice/service/java/com/android/server/art/ReasonMapping.java
+++ libartservice/service/java/com/android/server/art/ReasonMapping.java
@@ -195,6 +195,6 @@ public class ReasonMapping {
      */
     public static int getConcurrencyForReason(@NonNull @BatchDexoptReason String reason) {
         return SystemProperties.getInt("persist.device_config.runtime." + reason + "_concurrency",
-                SystemProperties.getInt("pm.dexopt." + reason + ".concurrency", 4 /* def */));
+                SystemProperties.getInt("pm.dexopt." + reason + ".concurrency", 6 /* def */));
     }
 }
